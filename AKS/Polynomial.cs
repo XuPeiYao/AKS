6370463931f1e94df78b614bcb176205521912d8
Pei Yao,Xu <u0124008@nkfust.edu.tw>
2016-04-01 14:49:32 +0800

    Init

---
+++ AKS/Polynomial.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace AKS {
+    public class Polynomial :ICloneable {
+        
+        public Dictionary<BigInteger, BigInteger> Data = new Dictionary<BigInteger, BigInteger>();
+
+        public object Clone() {
+            var result = new Polynomial();
+            Dictionary<BigInteger, BigInteger> data = new Dictionary<BigInteger, BigInteger>();
+            foreach (var kp in Data) data[kp.Key] = kp.Value;
+            result.Data = data;
+            return result;
+        }
+
+        public static Polynomial operator +(Polynomial A, Polynomial B) {
+            var result = (Polynomial)A.Clone();
+            foreach(var value in B.Data) {
+                if (!result.Data.ContainsKey(value.Key)) result.Data[value.Key] = 0;
+                result.Data[value.Key] += value.Value;
+                if (result.Data[value.Key] == 0) result.Data.Remove(value.Key);
+            }
+            return result;
+        }
+
+        public static Polynomial operator +(Polynomial A,BigInteger B) {
+            var b = new Polynomial();
+            b.Data[0] = B;
+            return A + b;
+        }
+
+        public static Polynomial operator *(Polynomial A, Polynomial B) {
+            var result = new Polynomial();
+            
+            foreach (var value0 in A.Data) {
+                foreach (var value1 in B.Data) {
+                    var NewPower = value0.Key + value1.Key;
+                    var NewCoeff = value0.Value * value1.Value;
+                    if (!result.Data.ContainsKey(NewPower)) result.Data[NewPower] = 0;
+                    result.Data[NewPower] += NewCoeff;
+                }
+            }
+
+            foreach(var kp in result.Data) {
+                if (kp.Value == 0) result.Data.Remove(kp.Key);
+            }
+
+            return result;
+        }
+
+        public static Polynomial operator *(Polynomial A, BigInteger B) {
+            var b = new Polynomial();
+            b.Data[0] = B;
+            return A * b;
+        }
+
+        public static Polynomial operator -(Polynomial A) {
+            return A * -1;
+        }
+        public static Polynomial operator -(Polynomial A, Polynomial B) {
+            return A + -B;
+        }
+    }
+}
---
+++ AKS/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKS {
+    class Program {
+        static void Main(string[] args) {
+            Stopwatch watch = new Stopwatch();//建構碼表用以計算耗時
+            watch.Start();
+            AKSRangePrime(BigInteger.Parse("1000")).ForEach(x=>Console.WriteLine(x));
+            watch.Stop();
+            Console.WriteLine("運算耗時:" + watch.Elapsed);
+            Console.ReadKey();
+        }
+
+        public static List<BigInteger> AKSRangePrime(BigInteger Max) {
+            List<BigInteger> list = new List<BigInteger>();
+            var a = new Polynomial(); ;
+            a.Data[0] = -1;
+            a.Data[1] = 1;
+            var a_ = (Polynomial)a.Clone();
+            for (int i = 2; i <= Max; i++) {
+                a *= a_;
+                if (list.Any(x => i % x == 0)) continue;
+
+                var b = new Polynomial(); ;
+                b.Data[0] = -1;
+                b.Data[i] = 1;                
+
+                if ((a - b).Data.All(x => x.Value % i == 0)) {
+                    list.Add(i);
+                }
+            }
+            return list;
+        }
+
+        public static bool IsPrime(BigInteger Value) {
+            var a = new Polynomial(); ;
+            a.Data[0] = -1;
+            a.Data[1] = 1;
+
+            var a_ = (Polynomial)a.Clone();
+            for (int i = 1; i < Value; i++) a_ *= a;
+
+            var b = new Polynomial(); ;
+            b.Data[Value] = 1;
+            b.Data[0] = -1;
+
+            var result = (a_ - b);
+
+            return result.Data.Where(x => x.Key > 0).All(x => x.Value % Value == 0);
+        }
+    }
+}
